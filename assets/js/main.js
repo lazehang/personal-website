5694e3674de6b356a645d7669292e4335abdb76e
lazehang <lazehang@gmail.com>
2018-07-26 23:24:45 +0800

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,129 @@
+/*
+	Photon by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+if (window.location.hostname === "lazehang.com") {
+    window.location.hostname = "www.lazehang.com";
+}
+
+(function($) {
+
+    var $window = $(window),
+        $body = $('body');
+
+    // Breakpoints.
+    breakpoints({
+        xlarge: ['1141px', '1680px'],
+        large: ['981px', '1140px'],
+        medium: ['737px', '980px'],
+        small: ['481px', '736px'],
+        xsmall: ['321px', '480px'],
+        xxsmall: [null, '320px']
+    });
+
+    // Play initial animations on page load.
+    $window.on('load', function() {
+        window.setTimeout(function() {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
+
+    // Scrolly.
+    $('.scrolly').scrolly();
+
+})(jQuery);
+
+/* ---- particles.js config ---- */
+
+
+/* Text decoder */
+
+// ——————————————————————————————————————————————————
+// TextScramble
+// ——————————————————————————————————————————————————
+
+class TextScramble {
+    constructor(el) {
+        this.el = el
+        this.chars = '!<>-_\\/[]{}—=+*^?#________'
+        this.update = this.update.bind(this)
+    }
+    setText(newText) {
+        const oldText = this.el.innerText
+        const length = Math.max(oldText.length, newText.length)
+        const promise = new Promise((resolve) => this.resolve = resolve)
+        this.queue = []
+        for (let i = 0; i < length; i++) {
+            const from = oldText[i] || ''
+            const to = newText[i] || ''
+            const start = Math.floor(Math.random() * 40)
+            const end = start + Math.floor(Math.random() * 40)
+            this.queue.push({ from, to, start, end })
+        }
+        cancelAnimationFrame(this.frameRequest)
+        this.frame = 0
+        this.update()
+        return promise
+    }
+    update() {
+        let output = ''
+        let complete = 0
+        for (let i = 0, n = this.queue.length; i < n; i++) {
+            let { from, to, start, end, char } = this.queue[i]
+            if (this.frame >= end) {
+                complete++
+                output += to
+            } else if (this.frame >= start) {
+                if (!char || Math.random() < 0.28) {
+                    char = this.randomChar()
+                    this.queue[i].char = char
+                }
+                output += `<span class="dud">${char}</span>`
+            } else {
+                output += from
+            }
+        }
+        this.el.innerHTML = output
+        if (complete === this.queue.length) {
+            this.resolve()
+        } else {
+            this.frameRequest = requestAnimationFrame(this.update)
+            this.frame++
+        }
+    }
+    randomChar() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+}
+
+// ——————————————————————————————————————————————————
+// Example
+// ——————————————————————————————————————————————————
+
+const phrases = [
+    'Laze Hang',
+    'Mangal Hang',
+    'Coder',
+    'WebDeveloper',
+    'React',
+    'Node Js',
+    'JavaScript',
+    'Css3',
+    'HTML5',
+    'Php',
+    'JQuery'
+]
+
+const el = document.querySelector('.text')
+const fx = new TextScramble(el)
+let counter = 0
+const next = () => {
+    fx.setText(phrases[counter]).then(() => {
+        setTimeout(next, 1500)
+    })
+    counter = (counter + 1) % phrases.length
+}
+
+next();
